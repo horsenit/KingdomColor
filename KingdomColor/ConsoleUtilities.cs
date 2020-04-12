e24e3a9a33e782f8c34187294024b542b47a3617
horsenit <gon+ghorse@gonbon.com>
2020-04-12 02:15:49 -0400

    Refactor console commands.

---
+++ KingdomColor/ConsoleUtilities.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+
+namespace KingdomColor
+{
+    class ConsoleUtilities
+    {
+        public static T FindObjectByIdName<T>(string idName) where T : MBObjectBase
+        {
+            T obj = MBObjectManager.Instance.GetObject<T>(idName);
+            if (obj == null)
+            {
+                foreach (T candidate in MBObjectManager.Instance.GetObjectTypeList<T>())
+                {
+                    if (candidate.GetName().ToString().Equals(idName, StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        obj = candidate;
+                        break;
+                    }
+                }
+            }
+            return obj;
+        }
+
+        // awkward parser, inputting "you   a'b    c'd'" => "you", "a", "b c", "d", ""
+        // game throws away quotes, presplits on spaces, oh well
+        public static List<string> Resplit(List<string> args)
+        {
+            var concat = string.Join(" ", args);
+            var output = new List<string>();
+            var current = new StringBuilder();
+            bool isEscape = false;
+            bool isQuote = false;
+            bool isSpace = true;
+            foreach (var c in concat)
+            {
+                if (isEscape)
+                {
+                    current.Append(c);
+                    isEscape = false;
+                    continue;
+                }
+                switch (c)
+                {
+                    case '\\':
+                        isEscape = true;
+                        isSpace = false;
+                        break;
+                    case '\'':
+                        if (isQuote)
+                        {
+                            output.Add(current.ToString());
+                            current.Clear();
+                            isQuote = false;
+                            isSpace = true;
+                        }
+                        else
+                        {
+                            isSpace = false;
+                            isQuote = true;
+                        }
+                        break;
+                    case ' ':
+                    case '\n':
+                    case '\t':
+                        if (isQuote)
+                        {
+                            current.Append(c);
+                        }
+                        else
+                        {
+                            if (!isSpace)
+                            {
+                                output.Add(current.ToString());
+                                current.Clear();
+                            }
+                            isSpace = true;
+                        }
+                        break;
+                    default:
+                        current.Append(c);
+                        isSpace = false;
+                        break;
+                }
+            }
+            if (!isSpace)
+                output.Add(current.ToString());
+            return output;
+        }
+
+        public static string GetObjectList<T>() where T : MBObjectBase
+        {
+            var output = new StringBuilder($"\nList of {nameof(T)}s\n==============================\n");
+            foreach (var obj in MBObjectManager.Instance.GetObjectTypeList<T>())
+                output.Append($" Id: {obj.StringId}, Name: '{obj.GetName()}'\n");
+            return output.ToString();
+        }
+    }
+}
--- KingdomColor/KingdomColor.cs
+++ KingdomColor/KingdomColor.cs
@@ -151,49 +151,6 @@ namespace KingdomColor
             }
         }
 
-        [CommandLineFunctionality.CommandLineArgumentFunction("set_kingdom_color", "kingdomcolor")]
-        public static string Console_SetKingdomColor(List<string> args)
-        {
-            if (args.Count == 1 && args[0] == "colors")
-            {
-                string output = $"\nAvailable colors\n==============================\n";
-                foreach (var paletteEntry in BannerManager.ColorPalette)
-                {
-                    var c = Color.FromUint(paletteEntry.Value.Color);
-                    output += $" Id: {paletteEntry.Key}, {c.ToString()}, rgba({(int)(c.Red * 255f)}, {(int)(c.Green * 255)}, {(int)(c.Blue * 255)}, {c.Alpha})\n";
-                }
-                return output;
-            }
-            else if (args.Count == 1 && args[0] == "kingdoms")
-            {
-                string output = "\nAvailable kingdoms\n==============================\n";
-                foreach (var objectType in MBObjectManager.Instance.GetObjectTypeList<Kingdom>())
-                    output = output + $" Id: {objectType.StringId} Name: {objectType.Name}\n";
-                return output;
-            }
-            else if (args.Count < 3)
-            {
-                return "Usage: \"kingdomcolor.set_kingdom_color [KingdomId] [ColorId] [ColorId] [UniformColor] [UniformColor]\"\nUniformColors are optional and can be an HTML color ('#ffffff') or color id\nUse \"kingdomcolor.set_kingdom_color colors/kingdoms\" to list available colors or kingdoms";
-            }
-            var kingdom = MBObjectManager.Instance.GetObject<Kingdom>(args[0]);
-            if (kingdom == null) return "Couldn't find kingdom.";
-            int bannerColor;
-            int bannerColor2;
-            if (!int.TryParse(args[1], out bannerColor)) return "Invalid color1 specified";
-            if (!int.TryParse(args[2], out bannerColor2)) return "Invalid color2 specified";
-            uint primaryBannerColor = BannerManager.GetColor(bannerColor);
-            uint secondaryBannerColor = BannerManager.GetColor(bannerColor2);
-            uint? color1 = null;
-            uint? color2 = null;
-            if (args.Count >= 4)
-            {
-                color1 = ParseUniformColor(args[3]);
-                color2 = ParseUniformColor(args[4]);
-            }
-            Instance.SetKingdomColors(kingdom, primaryBannerColor, secondaryBannerColor, color1 ?? primaryBannerColor, color2 ?? secondaryBannerColor);
-            return $"Set {kingdom.Name} colors. Open and close the Clan page to take effect.";
-        }
-        
         protected override void OnSubModuleLoad()
         {
             base.OnSubModuleLoad();
